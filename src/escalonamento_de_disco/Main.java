f006a741e8452c032dc87a0ef13fa927fca0f67f
Siga Mais <Siga Mais@DESKTOP-MQ0DKDS>
2018-08-07 20:17:16 -0300

    Apresentação na Sala

--- src/escalonamento_de_disco/Main.java
+++ src/escalonamento_de_disco/Main.java
@@ -3,11 +3,16 @@ package escalonamento_de_disco;
 public class Main {
 
 	public static void main(String[] args) throws InterruptedException {
-//		(new SSTF()).run();
+		//Kenedy
 //		(new FCFS()).run();
-		(new C_LOOK()).run();
-//		(new LOOK()).run();
+//		(new SSTF()).run();
+		// Cesar
+//		(new C_LOOK()).run();
 		(new SCAN()).run();
+		// Andre
+
 //		(new C_SCAN()).run();
+//		(new LOOK()).run();
+
 	}
 }
